b41df70cff8eb1aacd44664c2c8fc57e65363121
kingofdead6 <ny_kahlouche@esi.dz>
2025-07-04 19:57:20 +0100

    Added new features

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import userModel from '../models/userModel.js';
+import userModel from '../Models/userModel.js';
 
 export const authMiddleware = async (req, res, next) => {
   try {
